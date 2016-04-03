56dca76c5e3a3af9c77f1952825b82490c83bd36
horaceheaven <hheaven@medullan.com>
2016-04-02 20:23:57 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path =  require('path');
+var config = require('./config.js');
+
+var visitorsData = {};
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'public/')));
+
+
+app.get(/\/(about|contact)?$/, function(req, res) {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+app.get('/dashboard', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views/dashboard/html'));
+});
+
+io.on('connection', function(socket) {
+    socket.on('disconnect', function() {
+
+    });
+});
+
+http.listen(app.get('port'), function() {
+   console.log('listening on *:' + app.get('port'));
+});
